85b0295e90672344e823046b52d404dc8f9dfa16
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2021-12-19 00:15:22 -0800

    'ok'

--- resources/views/frontend/inc/gameitem.blade.php
+++ resources/views/frontend/inc/gameitem.blade.php
@@ -1,5 +1,5 @@
 
-<div class="item ">
+<div class="item " style="width: 100%">
   <div style="width: 100%; background: red; ">
       <div style=" " class="myflex ">
           <img class="gameItemcardimage" src="{{isset($imageurl)?$imageurl:''}}">
