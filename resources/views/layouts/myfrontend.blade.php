8891e6addd3a3ce0192729bb1fc5e9203adf248b
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-02-25 21:55:06 -0800

    ok

--- resources/views/layouts/myfrontend.blade.php
+++ resources/views/layouts/myfrontend.blade.php
@@ -22,5 +22,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="{{asset('frontend/css/mobileMediaQueries.css')}}">
+@include('myfrontend.inc.js')
 </body>
 </html>
