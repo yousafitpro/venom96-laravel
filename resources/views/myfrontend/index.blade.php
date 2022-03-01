e9cc81f08ff4b471bd6de4b9a67bc246e699aa6a
aRooj-saghaR <aroojsaghar648@gmail.com>
2022-03-01 16:52:43 +0400

    Home Page Updated

--- resources/views/myfrontend/index.blade.php
+++ resources/views/myfrontend/index.blade.php
@@ -10,7 +10,10 @@
         <div class="homegames">
                 <div class="inner">
                         <div class="mobile_games category">
-                        <h3><span>Popular Mobile Games</span></h3>
+                        <h3>
+                                <img src="{{asset('myimages/mbgames.png')}}" alt="">
+                                <span>Popular Mobile Games</span>
+                        </h3>
                         <ul class="list">
                                 <li>
                                         @include("myfrontend.inc.gamecard")
@@ -30,7 +33,10 @@
                         </ul>
                         </div>
                         <div class="pc_games category">
-                        <h3><span>Popular PC Games</span></h3>
+                        <h3>
+                                <img src="{{asset('myimages/pcgames.png')}}" alt="">
+                                <span>Popular PC Games</span>
+                        </h3>
                         <ul class="list">
                         <li>
                                 @include("myfrontend.inc.gamecard")
@@ -145,68 +151,96 @@
                                 <div class="home_features">
                                 <ul>
                                         <li>
-                                        <div>
-                                                <i class="fa icon fa-2x fa-search pb-3" aria-hidden="true"></i>
-                                                <h3>Find the Relevant Items</h3>
-                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
-                                        </div>
+                                                <div>
+                                                       <i class="fa icon fa-2x fa-search pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content"> 
+                                                          <h3>Find the Relevant Items</h3>
+                                                           <p>KALEOZ search and filters enable fast and accurate search.</p>
+                                                        </div>
+                                                        
+                                                </div>
                                         </li>
                                         <li>
-                                        <div>
-                                                <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
-                                                <h3>Safe & Secure</h3>
-                                                <p>No scam. No need others to vouch. No fraud and chargebacks. KALEOZ makes sure buyers gets the items purchased and seller gets his payments.</p>
-                                        </div>
+                                                <div>
+                                                        <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content">
+                                                                <h3>Safe & Secure</h3>
+                                                                <p>No scam. No need others to vouch. No fraud and chargebacks. KALEOZ makes sure buyers gets the items purchased and seller gets his payments.</p>
+                                                        </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <div>
-                                                <i class="fa icon fa-2x fa-university pb-3" aria-hidden="true"></i>
-                                                <h3>Low fees & Attractive rewards</h3>
-                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
-                                        </div>
+                                                <div>
+                                                        <i class="fa icon fa-2x fa-university pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content">
+                                                                <h3>Low fees & Attractive rewards</h3>
+                                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
+                                                        </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <div>
-                                                <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
-                                                <h3>24×7 Support</h3>
-                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
-                                        </div>
+                                                <div>
+                                                        <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content">
+                                                                <h3>24×7 Support</h3>
+                                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
+                                                        </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <div>
-                                                <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
-                                                <h3>Buy at Lower Price</h3>
-                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
-                                        </div>
+                                                <div>
+                                                        <i class="fa icon fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content">
+                                                                <h3>Buy at Lower Price</h3>
+                                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
+                                                        </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <div>
-                                                <i class="icon fa fa-2x fa-shield pb-3" aria-hidden="true"></i>
-                                                <h3>Earn Real Cash</h3>
-                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
-                                        </div>
+                                                <div>
+                                                        <i class="icon fa fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                        <div class="f_content">
+                                                                <h3>Earn Real Cash</h3>
+                                                                <p>KALEOZ search and filters enable fast and accurate search.</p>
+                                                        </div>
+                                                </div>
                                         </li>
                                 </ul>
                                 </div>
                                 <div class="home_guide">
                                 <ul>
                                         <li>
-                                        <a href="">
-                                                <h3>How To Buy</h3>
-                                                <p>Find out the buying guide here.</p>
-                                        </a>
+                                                <div>
+                                                     <i class="icon fa fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                    <div class="f_content">
+                                                        <a href="">
+                                                                <h3>How To Buy</h3>
+                                                                <p>Find out the buying guide here.</p>
+                                                        </a>
+                                                    </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <a href="">
-                                                <h3>How To Sell</h3>
-                                                <p>Find out the buying guide here.</p>
-                                        </a>
+                                                <div>
+                                                     <i class="icon fa fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                    <div class="f_content">
+                                                        <a href="">
+                                                                <h3>How To Sell</h3>
+                                                                <p>Find out the buying guide here.</p>
+                                                        </a>
+                                                    </div>
+                                                </div>
                                         </li>
                                         <li>
-                                        <a href="">
-                                                <h3>Beginner's Guide</h3>
-                                                <p>Find out the buying guide here.</p>
-                                        </a>
+                                                <div>
+                                                     <i class="icon fa fa-2x fa-shield pb-3" aria-hidden="true"></i>
+                                                    <div class="f_content">
+                                                        <a href="">
+                                                                <h3>Beginner's Guide</h3>
+                                                                <p>Find out the buying guide here.</p>
+                                                        </a>
+                                                    </div>
+                                                </div>
                                         </li>
                                 </ul>
                                 </div>
